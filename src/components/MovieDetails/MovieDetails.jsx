8c18498846604474ed0c868fff9c8deee7cc1772
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-09-18 14:19:41 +0200

    fixed MovieDetails

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,17 +1,16 @@
-// src/components/MovieDetails.js
-
-// MovieDetails.js
 import React, { useState, useEffect } from 'react';
 import { fetchMovieDetails } from 'services/api';
+import { useParams } from 'react-router-dom';
 
-export function MovieDetails({ match }) {
+export function MovieDetails() {
+  const { id } = useParams();
   const [movieDetails, setMovieDetails] = useState({});
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const details = await fetchMovieDetails(match.params.id);
+        const details = await fetchMovieDetails(id);
         setMovieDetails(details);
         setIsLoading(false);
       } catch (error) {
@@ -20,7 +19,7 @@ export function MovieDetails({ match }) {
     };
 
     fetchData();
-  }, [match.params.id]);
+  }, [id]);
 
   return (
     <div>
@@ -35,5 +34,41 @@ export function MovieDetails({ match }) {
     </div>
   );
 }
+
+// MovieDetails.js
+// import React, { useState, useEffect } from 'react';
+// import { fetchMovieDetails } from 'services/api';
+
+// export function MovieDetails({ match }) {
+//   const [movieDetails, setMovieDetails] = useState({});
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const details = await fetchMovieDetails(match.params.id);
+//         setMovieDetails(details);
+//         setIsLoading(false);
+//       } catch (error) {
+//         console.error('Error fetching movie details:', error);
+//       }
+//     };
+
+//     fetchData();
+//   }, [match.params.id]);
+
+//   return (
+//     <div>
+//       {isLoading ? (
+//         <p>Loading...</p>
+//       ) : (
+//         <div>
+//           <h1>{movieDetails.title}</h1>
+//           <p>{movieDetails.overview}</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
 
 
