d7f845086b05d2506bf9cc4517f8ee9bfdb7bd45
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-09-23 15:52:05 +0200

    added lazy&Suspense

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,39 +1,34 @@
 // import React, { Suspense } from 'react';
-import React from 'react';
+import { lazy, Suspense } from 'react';
 import { Route, Outlet, Routes, Link } from 'react-router-dom';
-import { Home } from '../pages/Home/Home';
-import { Movies } from '../pages/Movies/Movies';
-import { MovieDetails } from '../MovieDetails/MovieDetails';
-import { Cast } from '../Cast/Cast';
-import { Reviews } from '../Reviews/Reviews';
-import { NavWrap, RoutesWrap } from '../App/AppStyled.styled'
+import { NavWrap, RoutesWrap } from '../App/AppStyled.styled';
+
+const Home = lazy(() => import ('../pages/Home/Home'));
+const Movies = lazy(() => import ('../pages/Movies/Movies'));
+const MovieDetails = lazy(() => import ('../MovieDetails/MovieDetails'));
+const Cast = lazy(() => import ('../Cast/Cast'));
+const Reviews = lazy(() => import ('../Reviews/Reviews'));
 
 export const App = () => {
   return (
     <div> 
       <NavWrap>
-        <Link to="/" end>Home</Link>
-        <Link to="/movies">Movies</Link>
-      </NavWrap>
+        <Link to="/" end='true'>Home</Link>
+        <Link to="/movies" end='true'>Movies</Link>
+      </NavWrap> 
 
-      <RoutesWrap>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:id" element={<MovieDetails />} />
-          <Route path="/movies/:id/cast" element={<Cast />} />
-          <Route path="/movies/:id/reviews" element={<Reviews />} />
-          <Route path="/*" element={<Outlet />} />
-        </Routes>
-      </RoutesWrap>
-
+      <Suspense fallback={<div>Loading...</div>}>
+        <RoutesWrap>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/movies/:id" element={<MovieDetails />} />
+            <Route path="/movies/:id/cast" element={<Cast />} />
+            <Route path="/movies/:id/reviews" element={<Reviews />} />
+            <Route path="/*" element={<Outlet />} />
+          </Routes>
+        </RoutesWrap>
+      </Suspense>
     </div>
-
-
-    // <Router>
-    //   <Suspense fallback={<div>Loading...</div>}>
-    //     <RoutesItem />
-    //   </Suspense>
-    // </Router>
   );
 };
