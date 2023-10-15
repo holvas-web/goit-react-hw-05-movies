4bebf53a0525baebac8d5a33bea7332cfa3588e2
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-10-15 20:54:13 +0200

    loader-

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -30,25 +30,6 @@ export function fetchMovieReviews(movieId) {
 }
 
 
-
-// import axios from "axios";
-
-// axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
-// const KEY = '08685f82d21c93cd92857dcadddfeb71';
-
-// const api = {
-//     fetchTrendingMovies(currentPage) {
-//         return axios
-//             .get(`trending/movie/week?api_key=${KEY}&page=${currentPage}`)
-//             .then(response => console.log(response.data.results));
-        
-//     }
-// }
-
-// export default api;
-
-
-
 // // api.js
 // import axios from 'axios';
 
