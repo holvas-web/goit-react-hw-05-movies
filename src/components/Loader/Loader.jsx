f4e88c56a74807d78602fb5e20f87d4d4e4b4456
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-10-15 20:53:40 +0200

    loader-

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,18 @@
-import { Audio } from 'react-loader-spinner';
+// import { Audio } from 'react-loader-spinner';
 
-export default function Loader() {
-  return ( 
-    <Audio
-      height="80"
-      width="80"
-      radius="9"
-      color="green"
-      ariaLabel="loading"
-      wrapperStyle
-      wrapperClass
-    />
-  )
-};
+// export default function Loader() {
+//   return ( 
+//     <Audio
+//       height="80"
+//       width="80"
+//       radius="9"
+//       color="green"
+//       ariaLabel="loading"
+//       wrapperStyle
+//       wrapperClass
+//     />
+//   )
+// };
 
 
 // import Loader from 'react-loader-spinner';
